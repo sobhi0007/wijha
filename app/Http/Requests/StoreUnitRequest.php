ece8b9c8bd3a942890fd2c2f54c91d3765f63002
sobhi <mohaamed.sobhii@gmail.com>
2023-06-25 10:04:10 +0300

    make images units accept webp

--- app/Http/Requests/StoreUnitRequest.php
+++ app/Http/Requests/StoreUnitRequest.php
@@ -58,7 +58,7 @@ class StoreUnitRequest extends FormRequest
             'extra.parking_information'  => 'nullable',
             'extra.wifi_information'     => 'nullable',
             'images'                     => 'required',
-            'images.*'                   => 'nullable|mimes:jpeg,png,jpg,gif,svg,pdf|max:2048',
+            'images.*'                   => 'nullable|mimes:jpeg,png,jpg,gif,svg,pdf,webp|max:2048',
             'fromDate'                   => 'sometimes|required',
             'fromDate.*'                 => 'sometimes|required|date',
             'toDate'                     => 'sometimes|required',
